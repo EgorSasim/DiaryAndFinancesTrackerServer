665c957dc77e0b434a989cc99b9b9ce42a87f9a0
Egor Sasim <egorsasim@mail.ru>
2023-12-11 01:39:48 +0300

    implement lists request, DFTS-2

--- src/models/list/list.controller.ts
+++ src/models/list/list.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { List } from '@prisma/client';
 import { ListService } from 'src/models/list/list.service';
 
@@ -6,10 +6,34 @@ import { ListService } from 'src/models/list/list.service';
 export class ListController {
   constructor(private listService: ListService) {}
 
+  @Get('lists')
+  async getLists(@Query() params: { spaceId: string }): Promise<List[]> {
+    const spaceId: number = +params.spaceId;
+    return this.listService.lists({ where: { spaceId } });
+  }
+
   @Post('list')
-  async createList(@Req() request: Request, @Body() listData): Promise<List> {
-    console.log('request: ', request);
-    console.log('list data: ', listData);
-    return;
+  async createList(
+    @Body()
+    listData: {
+      title: string;
+      spaceId?: number;
+      folderId?: number;
+    },
+  ): Promise<List> {
+    const { title, spaceId, folderId } = listData;
+    if (spaceId) {
+      return this.listService.createList({
+        title: title,
+        space: { connect: { id: spaceId } },
+      });
+    }
+
+    if (folderId) {
+      return this.listService.createList({
+        title: title,
+        folder: { connect: { id: folderId } },
+      });
+    }
   }
 }
